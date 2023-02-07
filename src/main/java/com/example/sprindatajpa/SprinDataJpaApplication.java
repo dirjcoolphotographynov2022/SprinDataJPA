0f9d01a592ab732a91102300015acbb52d4b0a2e
Dhiraj <dhirajgadwal@gmail.com>
2023-02-07 09:18:25 +0530

    Initial commit

---
+++ src/main/java/com/example/sprindatajpa/SprinDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprindatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sprindatajpa/SprinDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprindatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
